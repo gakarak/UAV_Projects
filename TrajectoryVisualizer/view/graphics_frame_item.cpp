01e9df87b6d7c349fb8557fa9f462e280f204166
Vlad Blazhko <mr_plum@mail.ru>
2016-05-19 16:28:50 +0300

    Added display selection

--- TrajectoryVisualizer/view/graphics_frame_item.cpp
+++ TrajectoryVisualizer/view/graphics_frame_item.cpp
@@ -1,13 +1,39 @@
 #include "graphics_frame_item.h"
 
+#include <QDebug>
+
 using namespace viewpkg;
 
 GraphicsFrameItem::GraphicsFrameItem()
-    : GraphicsMapItem()
+    : GraphicsMapItem(), selected(false)
 {
 }
 
 GraphicsFrameItem::GraphicsFrameItem(const QPixmap &frame, int frame_num)
-    : GraphicsMapItem(frame), frame_num(frame_num)
+    : GraphicsMapItem(frame), frame_num(frame_num), selected(false)
 {
+}
+
+void GraphicsFrameItem::mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event)
+{
+    selected = !selected;
+
+    //selected item always have bounds
+    bound_item.setVisible(selected);
+}
+
+void GraphicsFrameItem::hoverEnterEvent(QGraphicsSceneHoverEvent *event)
+{
+    if (!selected)
+    {
+        GraphicsMapItem::hoverEnterEvent(event);
+    }
+}
+
+void GraphicsFrameItem::hoverLeaveEvent(QGraphicsSceneHoverEvent *event)
+{
+    if (!selected)
+    {
+        GraphicsMapItem::hoverLeaveEvent(event);
+    }
 }
--- TrajectoryVisualizer/view/graphics_frame_item.h
+++ TrajectoryVisualizer/view/graphics_frame_item.h
@@ -13,10 +13,21 @@ namespace viewpkg
         GraphicsFrameItem(const QPixmap &frame, int frame_num);
 
         void setFrameNumber(int frame_num)  { this->frame_num = frame_num; }
-        int  getFrameNumber()               { return frame_num; }
+        int  getFrameNumber() const         { return frame_num; }
+
+        bool isSelected() const             { return selected; }
+
+    protected:
+        void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event) override;
+
+        //if selected then hover events cannot hide bound_item
+        void hoverEnterEvent(QGraphicsSceneHoverEvent *event) override;
+        void hoverLeaveEvent(QGraphicsSceneHoverEvent *event) override;
 
     private:
         int frame_num;
+
+        bool selected;
     };
 
 }
