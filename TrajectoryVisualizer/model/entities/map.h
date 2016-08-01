0739671a4a218af4046f86bd9cfa29ed8bd8bef8
Vlad Blazhko <mr_plum@mail.ru>
2016-08-01 22:04:50 +0300

    center of image calculated in constructor

--- TrajectoryVisualizer/model/entities/map.h
+++ TrajectoryVisualizer/model/entities/map.h
@@ -16,9 +16,12 @@ namespace modelpkg
         Map(std::string filename, double x_m, double y_m, double angle, double m_per_px)
             : image( cv::imread(filename, cv::IMREAD_GRAYSCALE) ), pos_m(x_m, y_m),
               angle(angle), m_per_px(m_per_px)
-        {}
+        {
+          image_center = cv::Point2f(image.cols/2., image.rows/2.);
+        }
 
         cv::Mat image;
+        cv::Point2f image_center;
         cv::Point2f pos_m; //in meters
         double angle;
         double m_per_px;
