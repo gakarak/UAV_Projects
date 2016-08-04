cc7ed4eca190c064ca410c8f9f4cb3a291c157c5
Vlad Blazhko <mr_plum@mail.ru>
2016-08-04 16:08:38 +0300

    Changed interface in progress bar notifier

--- TrajectoryVisualizer/algorithms/progress_bar_notifier.cpp
+++ TrajectoryVisualizer/algorithms/progress_bar_notifier.cpp
@@ -1,13 +1,45 @@
 #include "progress_bar_notifier.h"
 
+#include <iostream>
+
 using namespace algorithmspkg;
 
-void algorithmspkg::ProgressBarNotifier::setCallback(NotifyFunc callback)
+void ProgressBarNotifier::setProgressNotifier(ProgressNotifier progress_notifier)
 {
-  this->callback = callback;
+  this->progress_notifier = progress_notifier;
+}
+
+void ProgressBarNotifier::setTitleSetter(TitleSetter title_setter)
+{
+  this->title_setter = title_setter;
+}
+
+
+
+
+void ProgressBarNotifier::setProgressBarTitle(std::string title, bool isNeedToReset)
+{
+  if (title_setter != nullptr)
+  {
+    title_setter(title, isNeedToReset);
+  }
+  else
+  {
+    //do nothing
+    //so client doesn't care about progress bar existence
+  }
 }
 
 void ProgressBarNotifier::notifyProgressBar(int value, int maximum, int minimum)
 {
-  callback(value, maximum, minimum);
+  if (progress_notifier != nullptr)
+  {
+    progress_notifier(value, maximum, minimum);
+  }
+  else
+  {
+    //std::clog << "Warning: callback is nullptr!" << endl;
+    //do nothing
+    //so client doesn't care about progress bar existence
+  }
 }
--- TrajectoryVisualizer/algorithms/progress_bar_notifier.h
+++ TrajectoryVisualizer/algorithms/progress_bar_notifier.h
@@ -2,6 +2,7 @@
 #define PROGRESSBARNOTIFIER_H
 
 #include <tr1/functional>
+#include <string>
 
 namespace algorithmspkg
 {
@@ -9,21 +10,32 @@ namespace algorithmspkg
 class ProgressBarNotifier
 {
  public:
-  using NotifyFunc = std::tr1::function<void(int, int, int)>;
+  using ProgressNotifier = std::tr1::function<void(int, int, int)>;
+  using TitleSetter = std::tr1::function<void(std::string, bool)>;
 
   ProgressBarNotifier() = default;
+
   /**
-   * @brief setCallback
-   * @param callback - function, which accept value, maximum, minimum and notify
-   * a progress bar
+   * @brief setTitleSetter
+   * @param title_setter - function, which accept task_name and isNeedToReset
+   * and set this params to progress bar
    */
-  void setCallback(NotifyFunc callback);
+  void setTitleSetter(TitleSetter title_setter);
+
+  /**
+   * @brief setProgressNotifier
+   * @param progress_notifier - function, which accept value, maximum, minimum
+   * and notifing the progress bar
+   */
+  void setProgressNotifier(ProgressNotifier progress_notifier);
 
  protected:
+  void setProgressBarTitle(std::string title, bool isNeedToReset = true);
   void notifyProgressBar(int value, int maximum = 100, int minimum = 0);
 
  private:
-  NotifyFunc callback;
+  TitleSetter title_setter;
+  ProgressNotifier progress_notifier;
 };
 
 }
