78f4a4063900119da102711aeacd3b76c5faa193
Vlad Blazhko <mr_plum@mail.ru>
2016-08-09 21:33:42 +0300

    added utility functions

---
+++ TrajectoryVisualizer/utils/gradient_density.cpp
@@ -0,0 +1,51 @@
+#include "gradient_density.h"
+
+#include <opencv2/imgproc.hpp>
+
+double utils::cv::gradientDensity(::cv::Mat image)
+{
+  ::cv::Point2f center(image.cols / 2, image.rows / 2);
+  int radius = std::min(image.cols, image.rows) / 2;
+
+  ::cv::Mat grad_x;
+  ::cv::Mat grad_y;
+
+  ::cv::Sobel(image, grad_x, CV_16S, 1, 0);
+  ::cv::Sobel(image, grad_y, CV_16S, 0, 1);
+
+  ::cv::Mat abs_grad_x;
+  ::cv::Mat abs_grad_y;
+
+  ::cv::convertScaleAbs(grad_x, abs_grad_x);
+  ::cv::convertScaleAbs(grad_y, abs_grad_y);
+
+  ::cv::Mat grad_value;
+  ::cv::addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad_value);
+
+  double grad_density = 0;
+  int pixel_count = 0;
+
+  for (int i = 0; i < grad_value.rows; i++)
+  {
+    for (int j = 0; j < grad_value.cols; j++)
+    {
+      if (sqrt(pow(i - center.y, 2) + pow(j - center.x, 2)) < radius)
+      {
+        grad_density += grad_value.at<uchar>(i, j);
+        pixel_count++;
+      }
+    }
+  }
+
+  return grad_density / pixel_count;
+}
+
+QPointF utils::cv::toQPointF(::cv::Point2f pt)
+{
+  return QPointF(pt.x, pt.y);
+}
+
+::cv::Point2f utils::cv::toPoint2f(QPointF pt)
+{
+  return ::cv::Point2f(pt.x(), pt.y());
+}
--- TrajectoryVisualizer/utils/gradient_density.h
+++ TrajectoryVisualizer/utils/gradient_density.h
@@ -1,63 +1,22 @@
 #ifndef GRADIENT_DENSITY_H
 #define GRADIENT_DENSITY_H
 
-#include <algorithm>
-
 #include <QPointF>
 
 #include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
 
 namespace utils
 {
 
-    namespace cv
-    {
+namespace cv
+{
 
-        double gradientDensity(::cv::Mat image)
-        {
-            ::cv::Point2f center(image.cols / 2, image.rows / 2);
-            int radius = std::min(image.cols, image.rows) / 2;
+  double gradientDensity(::cv::Mat image);
 
-            ::cv::Mat grad_x;
-            ::cv::Mat grad_y;
+  QPointF toQPointF(::cv::Point2f pt);
+  ::cv::Point2f toPoint2f(QPointF pt);
 
-            ::cv::Sobel(image, grad_x, CV_16S, 1, 0);
-            ::cv::Sobel(image, grad_y, CV_16S, 0, 1);
-
-            ::cv::Mat abs_grad_x;
-            ::cv::Mat abs_grad_y;
-
-            ::cv::convertScaleAbs(grad_x, abs_grad_x);
-            ::cv::convertScaleAbs(grad_y, abs_grad_y);
-
-            ::cv::Mat grad_value;
-            ::cv::addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad_value);
-
-            double grad_density = 0;
-            int pixel_count = 0;
-
-            for (int i = 0; i < grad_value.rows; i++)
-            {
-                for (int j = 0; j < grad_value.cols; j++)
-                {
-                    if (sqrt(pow(i - center.y, 2) + pow(j - center.x, 2)) < radius)
-                    {
-                        grad_density += grad_value.at<uchar>(i, j);
-                        pixel_count++;
-                    }
-                }
-            }
-
-            return grad_density / pixel_count;
-        }
-
-        QPointF toQPointF(::cv::Point2f pt)
-        {
-            return QPointF(pt.x, pt.y);
-        }
-
-    }
+}
 
 }
 
