c1bbacbeaf5aca9e489bec79b75db6d688124018
Vlad Blazhko <mr_plum@mail.ru>
2016-08-16 20:15:43 +0300

    ident refactored + when hover write frame num

--- TrajectoryVisualizer/view/graphics_frame_item.cpp
+++ TrajectoryVisualizer/view/graphics_frame_item.cpp
@@ -5,50 +5,52 @@
 using namespace viewpkg;
 
 GraphicsFrameItem::GraphicsFrameItem(QGraphicsItem *parent)
-    : GraphicsMapItem(parent), selected(false)
+  : GraphicsMapItem(parent), selected(false)
 {
 }
 
-GraphicsFrameItem::GraphicsFrameItem(const QPixmap &frame, int frame_num, QGraphicsItem *parent)
-    : GraphicsMapItem(frame, parent), frame_num(frame_num), selected(false)
+GraphicsFrameItem::GraphicsFrameItem(const QPixmap &frame, int frame_num,
+                                     QGraphicsItem *parent)
+  : GraphicsMapItem(frame, parent), frame_num(frame_num), selected(false)
 {
 }
 
 void GraphicsFrameItem::setSelected(bool value)
 {
-    if (selected != value)
-    {
-        selected = value;
-
-        //selected item always have bounds
-        bound_item.setVisible(selected);
-
-        emit stateChanged(frame_num, selected);
-    }
-}
-
-void GraphicsFrameItem::mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event)
-{
-    selected = !selected;
+  if (selected != value)
+  {
+    selected = value;
 
     //selected item always have bounds
     bound_item.setVisible(selected);
 
     emit stateChanged(frame_num, selected);
+  }
+}
+
+void GraphicsFrameItem::mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event)
+{
+  selected = !selected;
+
+  //selected item always have bounds
+  bound_item.setVisible(selected);
+
+  emit stateChanged(frame_num, selected);
 }
 
 void GraphicsFrameItem::hoverEnterEvent(QGraphicsSceneHoverEvent *event)
 {
-    if (!selected)
-    {
-        GraphicsMapItem::hoverEnterEvent(event);
-    }
+  qDebug() << "hovered: " << frame_num;
+  if (!selected)
+  {
+    GraphicsMapItem::hoverEnterEvent(event);
+  }
 }
 
 void GraphicsFrameItem::hoverLeaveEvent(QGraphicsSceneHoverEvent *event)
 {
-    if (!selected)
-    {
-        GraphicsMapItem::hoverLeaveEvent(event);
-    }
+  if (!selected)
+  {
+    GraphicsMapItem::hoverLeaveEvent(event);
+  }
 }
--- TrajectoryVisualizer/view/graphics_frame_item.h
+++ TrajectoryVisualizer/view/graphics_frame_item.h
@@ -6,35 +6,36 @@
 namespace viewpkg
 {
 
-    class GraphicsFrameItem : public QObject, public GraphicsMapItem
-    {
-        Q_OBJECT
+class GraphicsFrameItem : public QObject, public GraphicsMapItem
+{
+  Q_OBJECT
 
-    public:
-        GraphicsFrameItem(QGraphicsItem *parent = 0);
-        GraphicsFrameItem(const QPixmap &frame, int frame_num, QGraphicsItem *parent = 0);
+ public:
+  GraphicsFrameItem(QGraphicsItem *parent = 0);
+  GraphicsFrameItem(const QPixmap &frame, int frame_num,
+                    QGraphicsItem *parent = 0);
 
-        void setFrameNumber(int frame_num)  { this->frame_num = frame_num; }
-        int  getFrameNumber() const         { return frame_num; }
+  void setFrameNumber(int frame_num)  { this->frame_num = frame_num; }
+  int  getFrameNumber() const         { return frame_num; }
 
-        void setSelected(bool value);
-        bool isSelected() const             { return selected; }
+  void setSelected(bool value);
+  bool isSelected() const             { return selected; }
 
-    signals:
-        void stateChanged(int frame_num, bool isSelected);
+ signals:
+  void stateChanged(int frame_num, bool isSelected);
 
-    protected:
-        void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event) override;
+ protected:
+  void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event) override;
 
-        //if selected then hover events cannot hide bound_item
-        void hoverEnterEvent(QGraphicsSceneHoverEvent *event) override;
-        void hoverLeaveEvent(QGraphicsSceneHoverEvent *event) override;
+  //if selected then hover events cannot hide bound_item
+  void hoverEnterEvent(QGraphicsSceneHoverEvent *event) override;
+  void hoverLeaveEvent(QGraphicsSceneHoverEvent *event) override;
 
-    private:
-        int frame_num;
+ private:
+  int frame_num;
 
-        bool selected;
-    };
+  bool selected;
+};
 
 }
 
